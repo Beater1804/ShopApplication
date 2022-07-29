262f41aad5034bf19d88f850c2a2872d5c433d5e
Beater1804 <thanhdung12qh@gmail.com>
2022-07-29 15:40:29 +0700

    Update favorite into Firebase

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,10 +19,32 @@ class Product with ChangeNotifier {
     required this.imageUrl,
     this.isFavorite = false,
   });
+  void _setFavoriteValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
 
-  void toggleFavoriteStatus() {
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     //update the status of the button Favorite
     notifyListeners();
+    final url =
+        'https://shopappflutter-2de3c-default-rtdb.asia-southeast1.firebasedatabase.app/products/$id.json';
+
+    try {
+      final response = await http.patch(
+        Uri.parse(url),
+        body: json.encode({
+          'isFavorite': isFavorite,
+        }),
+      );
+      if (response.statusCode >= 400) {
+        _setFavoriteValue(oldStatus);
+      }
+    } catch (error) {
+      //backup data if error
+      _setFavoriteValue(oldStatus);
+    }
   }
 }
